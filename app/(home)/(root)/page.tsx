ad5472f719e684be2a458510130b6b37217b742e
Oscdivin <divineuwandu84@Gmail.com>
2024-09-11 23:11:16 +0100

    push code

---
+++ app/(home)/(root)/page.tsx
@@ -0,0 +1,35 @@
+export default async function page() {
+  // let data = await fetch('https://api.vercel.app/blog')
+  // let posts = await data.json()
+  return (
+    <div>
+      {/* {posts.map((post: any) => (
+        <li key={post.id}>{post.title} post</li>
+      ))} */}
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+      <h1>home</h1>
+    </div>
+  )
+}
